2f1ed2e668c8721a0e655932b73364649033ad58
M.P.Ross <mpross2@uw.edu>
2018-08-09 09:23:43 -0700

    Cleaned up directories again.

---
+++ lib/Phidget22_BLDCMotor_Python_Ex_20180726/BLDCMotor.py
@@ -0,0 +1,121 @@
+import sys
+import time 
+from Phidget22.Devices.BLDCMotor import *
+from Phidget22.PhidgetException import *
+from Phidget22.Phidget import *
+from Phidget22.Net import *
+
+try:
+    ch = BLDCMotor()
+except RuntimeError as e:
+    print("Runtime Exception %s" % e.details)
+    print("Press Enter to Exit...\n")
+    readin = sys.stdin.read(1)
+    exit(1)
+
+def BLDCMotorAttached(e):
+    try:
+        attached = e
+        print("\nAttach Event Detected (Information Below)")
+        print("===========================================")
+        print("Library Version: %s" % attached.getLibraryVersion())
+        print("Serial Number: %d" % attached.getDeviceSerialNumber())
+        print("Channel: %d" % attached.getChannel())
+        print("Channel Class: %s" % attached.getChannelClass())
+        print("Channel Name: %s" % attached.getChannelName())
+        print("Device ID: %d" % attached.getDeviceID())
+        print("Device Version: %d" % attached.getDeviceVersion())
+        print("Device Name: %s" % attached.getDeviceName())
+        print("Device Class: %d" % attached.getDeviceClass())
+        print("\n")
+    except PhidgetException as e:
+        print("Phidget Exception %i: %s" % (e.code, e.details))
+        print("Press Enter to Exit...\n")
+        readin = sys.stdin.read(1)
+        exit(1)   
+    
+def BLDCMotorDetached(e):
+    detached = e
+    try:
+        print("\nDetach event on Port %d Channel %d" % (detached.getHubPort(), detached.getChannel()))
+    except PhidgetException as e:
+        print("Phidget Exception %i: %s" % (e.code, e.details))
+        print("Press Enter to Exit...\n")
+        readin = sys.stdin.read(1)
+        exit(1)   
+
+def ErrorEvent(e, eCode, description):
+    print("Error %i : %s" % (eCode, description))
+
+def VelocityUpdateHandler(e, velocity):
+    print("Velocity: %f" % velocity)
+
+try:
+    ch.setOnAttachHandler(BLDCMotorAttached)
+    ch.setOnDetachHandler(BLDCMotorDetached)
+    ch.setOnErrorHandler(ErrorEvent)
+
+    ch.setOnVelocityUpdateHandler(VelocityUpdateHandler)
+
+    # Please review the Phidget22 channel matching documentation for details on the device
+    # and class architecture of Phidget22, and how channels are matched to device features.
+
+    # Specifies the serial number of the device to attach to.
+    # For VINT devices, this is the hub serial number.
+    #
+    # The default is any device.
+    #
+    # ch.setDeviceSerialNumber(<YOUR DEVICE SERIAL NUMBER>) 
+
+    # For VINT devices, this specifies the port the VINT device must be plugged into.
+    #
+    # The default is any port.
+    #
+    # ch.setHubPort(0)
+
+    # Specifies which channel to attach to.  It is important that the channel of
+    # the device is the same class as the channel that is being opened.
+    #
+    # The default is any channel.
+    #
+    # ch.setChannel(0)
+
+    # In order to attach to a network Phidget, the program must connect to a Phidget22 Network Server.
+    # In a normal environment this can be done automatically by enabling server discovery, which
+    # will cause the client to discovery and connect to available servers.
+    #
+    # To force the channel to only match a network Phidget, set remote to 1.
+    #
+    # Net.enableServerDiscovery(PhidgetServerType.PHIDGETSERVER_DEVICEREMOTE);
+    # ch.setIsRemote(1)
+
+    print("Waiting for the Phidget BLDCMotor Object to be attached...")
+    ch.openWaitForAttachment(5000)
+except PhidgetException as e:
+    print("Phidget Exception %i: %s" % (e.code, e.details))
+    print("Press Enter to Exit...\n")
+    readin = sys.stdin.read(1)
+    exit(1)
+
+print("Setting Target Velocity to 1 for 5 seconds...\n")
+ch.setTargetVelocity(0.1)
+time.sleep(5)
+
+print("Setting Target Velocity to -1 for 5 seconds...\n")
+ch.setTargetVelocity(-0.1)
+time.sleep(5)
+
+print("Setting Target Velocity to 0 for 5 seconds...\n")
+ch.setTargetVelocity(0)
+time.sleep(5)
+
+try:
+    ch.close()
+except PhidgetException as e:
+    print("Phidget Exception %i: %s" % (e.code, e.details))
+    print("Press Enter to Exit...\n")
+    readin = sys.stdin.read(1)
+    exit(1) 
+print("Closed BLDCMotor device")
+exit(0)
+                     
